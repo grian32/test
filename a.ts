0191bfe48fb20242fe66b3fa164b17f0450cd914
grian <54550982+grian32@users.noreply.github.com>
2021-07-06 15:23:26 +0300

    Update a.ts

--- a.ts
+++ a.ts
@@ -2,6 +2,7 @@
  * {@link https://www.google.com/}
  * [google]{@link https://www.google.com/}
  * {@link https://www.google.com/|google}
+ * {@link https://www.google.com/ google}
  * @param y number to be added to 3
  * @returns the added numbers 
  */
