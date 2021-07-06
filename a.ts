cc8b8035a18145f8d7bfdf185543bbd14fe704ca
grian <54550982+grian32@users.noreply.github.com>
2021-07-06 14:51:45 +0300

    Update a.ts

--- a.ts
+++ a.ts
@@ -1,7 +1,9 @@
 /**
  * {@link https://www.google.com/}
- * [google]{@link https://www.google.com/} 
+ * [google]{@link https://www.google.com/}
+ * {@link https://www.google.com/|google}
  * @param y number to be added to 3
+ * @returns the added numbers 
  */
 export function x(y: number): number {
   return 3 + y;
