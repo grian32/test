67e325c260823c2f0e9faf2ddf042957c83b867f
grian <54550982+grian32@users.noreply.github.com>
2021-07-10 12:34:18 +0300

    Update a.ts

--- a.ts
+++ a.ts
@@ -3,7 +3,10 @@
  * [google]{@link https://www.google.com/}
  * {@link https://www.google.com/|google}
  * {@link https://www.google.com/ google site}
+ * insert content:
  * {@link www.google.com  ] Hello [}
+ * test xss:
+ * {@link www.google.com ] <svg onload="alert(1)"></svg [}
  * @param y number to be added to 3
  * @returns the added numbers 
  */
