7a5262952da395e6ab4ccf4b2fc92fa514e2afb3
grian <54550982+grian32@users.noreply.github.com>
2021-09-07 10:52:16 +0300

    Update jsdoc.ts

--- jsdoc.ts
+++ jsdoc.ts
@@ -1,4 +1,25 @@
-export function x(y: (string) => number,
-  // TODO: check if bigint is needed and change as found
-  z: number
-) {}
+async function init(
+  cb: (conn: Deno.Conn) => Promise<void>,
+  // TODO(@littledivy): Make this toggleable with a build script?
+  dev: boolean = false,
+) {
+  if (!dev) await downloadRelease();
+  const listener = Deno.listen({ port: 34254, transport: "tcp" });
+  const process = Deno.run({
+    cmd: [dev ? "target/debug/deno_sdl2" : "./deno_sdl2"],
+    stderr: "inherit",
+    stdout: "inherit",
+  });
+  const conn = await listener.accept();
+  const reqBuf = await readStatus(conn);
+  switch (reqBuf) {
+    case 0:
+      // VIDEO_READY
+      await cb(conn);
+      break;
+    default:
+      break;
+  }
+
+  await process.status();
+}
