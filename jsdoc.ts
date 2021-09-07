e65f06c6ba1b7c9c805c07da38b678d51ee0d85e
grian <54550982+grian32@users.noreply.github.com>
2021-09-07 11:17:18 +0300

    Update jsdoc.ts

--- jsdoc.ts
+++ jsdoc.ts
@@ -1,3 +1,10 @@
+import { EventEmitter } from "https://deno.land/x/event@2.0.0/mod.ts"
+
+type WindowEvent = {
+  event: [Event];
+  draw: [];
+};
+
 export function init(
   cb: (conn: Deno.Conn) => Promise<void>,
   // TODO(@littledivy): Make this toggleable with a build script?
@@ -5,3 +12,5 @@ export function init(
 ) {
 
 }
+
+export class Canvas extends EventEmitter<WindowEvent> {}
